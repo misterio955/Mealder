4f2bcd5a7c540a5402d87672796ca7bf4ce9c7ac
Exatros <exatros@gmail.com>
2018-12-03 22:49:22 +0100

    Make spinner list

--- app/src/main/java/com/example/ideo/mealder/activities/AddMeal.java
+++ app/src/main/java/com/example/ideo/mealder/activities/AddMeal.java
@@ -1,19 +1,17 @@
 package com.example.ideo.mealder.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Environment;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
@@ -23,12 +21,17 @@ public class AddMeal extends AppCompatActivity {
     private static final int RESULT_LOAD_IMG = 20;
     private ImageView image_view;
 
+    private String[] measureItem = new String[]{"Szt.","ml","g"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
 
         image_view = findViewById(R.id.imageView);
+
+        makeListMeasure();
 
     }
 
@@ -58,6 +61,13 @@ public class AddMeal extends AppCompatActivity {
         } else {
             Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
         }
+    }
+    //Tworzenie listy rozwijanej
+    private void makeListMeasure (){
+
+        Spinner spinnerListMeasure = findViewById(R.id.measureSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, measureItem);
+        spinnerListMeasure.setAdapter(adapter);
     }
 }
 
