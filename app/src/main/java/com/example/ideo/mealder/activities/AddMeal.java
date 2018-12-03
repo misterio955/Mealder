022505a4fae12936427b6b565a863e8e3a41e8de
Exatros <exatros@gmail.com>
2018-12-03 14:33:10 +0100

    Creating a meal adding template

---
+++ app/src/main/java/com/example/ideo/mealder/activities/AddMeal.java
@@ -0,0 +1,15 @@
+package com.example.ideo.mealder.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ideo.mealder.R;
+
+public class AddMeal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meal);
+    }
+}
